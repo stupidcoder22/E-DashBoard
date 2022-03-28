7eadb3c619cad1b3dadf6874988da4076bb9c230
prateek <stupidcoder22@gmail.com>
2022-03-28 20:42:00 +0530

    login api development done

--- backend/index.js
+++ backend/index.js
@@ -6,10 +6,28 @@ const User = require("./db/User");
 
 app.use(express.json());
 app.use(cors());
+
 app.post("/register", async (req, res) => {
   let user = await new User(req.body);
   let data = await user.save();
+  data = data.toObject();
+  delete data.password;
   res.send(data);
+});
+
+app.post("/login", async (req, res) => {
+  let userdata = req.body;
+
+  if (userdata.email && userdata.password) {
+    const databaseuser = await User.findOne(userdata).select("-password");
+    if (databaseuser) {
+      res.send(databaseuser);
+    } else {
+      res.send("no data found");
+    }
+  } else {
+    res.send("no data found");
+  }
 });
 
 app.listen(1000);
