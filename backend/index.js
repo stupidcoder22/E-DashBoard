8e8e9eb2fbdb6fbf83c45272a91f53a6a2f79f7d
prateek <stupidcoder22@gmail.com>
2022-03-29 18:31:42 +0530

    search api integration done

--- backend/index.js
+++ backend/index.js
@@ -75,4 +75,16 @@ app.put("/update/:id", async (req, res) => {
   }
 });
 
+//search Product
+app.get("/search/:key", async (req, res) => {
+  let result = await Product.find({
+    $or: [
+      { name: { $regex: req.params.key } },
+      { company: { $regex: req.params.key } },
+      { category: { $regex: req.params.key } },
+    ],
+  });
+  res.send(result);
+});
+
 app.listen(1000);
