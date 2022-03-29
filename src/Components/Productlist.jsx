64562dad9f6e23ba33fb4c504e8f9e301f0839b0
prateek <stupidcoder22@gmail.com>
2022-03-29 19:50:40 +0530

    search api integrated with search bar

--- src/Components/Productlist.jsx
+++ src/Components/Productlist.jsx
@@ -25,9 +25,28 @@ const Productlist = () => {
     getProduct();
   };
 
+  const serchhandle = async (e) => {
+    let key = e.target.value;
+    if (key) {
+      let result = await fetch(`http://localhost:1000/search/${key}`);
+      result = await result.json();
+      if (result) {
+        setproduct(result);
+      }
+    } else {
+      getProduct();
+    }
+  };
+
   return (
     <div className="product-list">
       <h3>Product List</h3>
+      <input
+        type="text"
+        className="search"
+        placeholder="Search Product"
+        onChange={serchhandle}
+      />
       <ul className="heading">
         <li>S. No</li>
         <li>Product Name</li>
@@ -35,20 +54,24 @@ const Productlist = () => {
         <li>Category</li>
         <li>Operation</li>
       </ul>
-      {product.map((data, index) => {
-        return (
-          <ul key={index}>
-            <li>{index + 1}</li>
-            <li>{data.name}</li>
-            <li>{data.price}</li>
-            <li>{data.category}</li>
-            <li>
-              <button onClick={() => deleteproduct(data._id)}>Delete</button>
-              <Link to={`/update/${data._id}`}>Update</Link>
-            </li>
-          </ul>
-        );
-      })}
+      {product.length > 0 ? (
+        product.map((data, index) => {
+          return (
+            <ul key={index}>
+              <li>{index + 1}</li>
+              <li>{data.name}</li>
+              <li>{data.price}</li>
+              <li>{data.category}</li>
+              <li>
+                <button onClick={() => deleteproduct(data._id)}>Delete</button>
+                <Link to={`/update/${data._id}`}>Update</Link>
+              </li>
+            </ul>
+          );
+        })
+      ) : (
+        <h1>No Data Available</h1>
+      )}
     </div>
   );
 };
